d4b2f1a06935dcbfeeeb5c894e97997a9146986c
FBetulDemir <fatmabetuld@gmail.com>
2025-11-05 07:37:09 +0100

    Add frontend for login endpoint

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,34 +1,95 @@
 import { useState } from "react";
+import "../styles/login.css";
+import "../index.css";
+
+interface FormData {
+  username: string;
+  password: string;
+}
+
+const LS_KEY = "jwt";
 
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [formData, setFormData] = useState<FormData>({
+    username: "",
+    password: "",
+  });
 
-  function handleSubmit(e: React.FormEvent) {}
+  const [loginErrorMessage, setLoginErrorMessage] = useState<string>("");
+  const [loginSuccessMessage, setLoginSuccessMessage] = useState<string>("");
+  //   const [username, setUsername] = useState<string>("");
+  //   const [password, setPassword] = useState<string>("");
+  // const [users, setUsers] = useState<UserResponse[]>([])
+
+  const handleSubmitLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoginErrorMessage("");
+    setLoginSuccessMessage("");
+
+    try {
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (!response.ok) {
+        setLoginErrorMessage("Felaktigt användarnamn eller lösenord!");
+        return;
+      }
+
+      // Servern skickar tillbaka ett objekt: { success: boolean, token?: string }
+      // TODO: validera med Zod att data variabeln matchar objektet
+      const data = await response.json();
+
+      if (data.success) {
+        const jwt: string = data.token;
+        localStorage.setItem(LS_KEY, jwt);
+
+        console.log("Inloggningen lyckades", formData.username);
+        setLoginSuccessMessage("Inloggningen lyckades!");
+      } else {
+        setLoginErrorMessage("Felaktigt användarnamn eller lösenord!");
+        localStorage.removeItem(LS_KEY);
+      }
+    } catch (error) {
+      setLoginErrorMessage("Nätverksfel. Försök igen.");
+    }
+  };
+
   return (
     <div className="login-container">
       <div className="login-card">
         <h1>login</h1>
-        <form onSubmit={handleSubmit} className="space-y-6">
+        <form onSubmit={handleSubmitLogin} className="login-form">
           <input
             type="text"
             placeholder="Användarnamn"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="w-full bg-transparent border-white/30 text-white placeholder:text-white/50 focus:border-[#f7a73f]"
+            value={formData.username}
+            onChange={(event) =>
+              setFormData({ ...formData, username: event.target.value })
+            }
           />
 
           <input
             type="password"
             placeholder="Lösenord"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className=""
+            value={formData.password}
+            onChange={(event) =>
+              setFormData({ ...formData, password: event.target.value })
+            }
           />
 
-          <button type="submit" className="">
+          <button type="submit" className="btn submit-btn">
             Logga in
           </button>
+          {loginErrorMessage && (
+            <span className="msg error"> {loginErrorMessage} </span>
+          )}
+          {loginSuccessMessage && (
+            <span className="msg success"> {loginSuccessMessage} </span>
+          )}
         </form>
       </div>
     </div>
